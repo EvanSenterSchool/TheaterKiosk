478bbc3f0186176e364661654570bf0df12bdca4
614925 <senter.614925@swl.k12.oh.us>
2024-09-26 08:57:13 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int personAge;
+        int wristBand_Min_Age = 21;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age.");
+
+        if (scan.hasNextInt()) {
+
+            personAge = scan.nextInt();
+
+            if (personAge <= 120 && personAge >= wristBand_Min_Age)
+                System.out.println("You get a wristband");
+
+            else {
+                System.out.println("You are not old enough");
+            }
+        }
+        else
+            System.out.println("You have entered the wrong data type");
     }
 }
